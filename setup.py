b01a998edfd305239a0f6ca733c0e8cb3fdc6d78
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2017-10-05 09:57:10 +0200

    setup: don't claim support for Python 3
    Signed-off-by: Jacopo Notarstefano <jacopo.notarstefano@gmail.com>

--- setup.py
+++ setup.py
@@ -93,8 +93,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
