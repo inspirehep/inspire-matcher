a279ff1bcfc4c899bfcf6c1d4997681be446ae53
Micha Moskovic <michamos@gmail.com>
2022-04-26 19:08:02 +0200

    validators: be more tolerant for titles with math

--- inspire_matcher/utils.py
+++ inspire_matcher/utils.py
@@ -3,8 +3,7 @@
 # This file is part of INSPIRE.
 # Copyright (C) 2014-2017 CERN.
 #
-# INSPIRE is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# INSPIRE is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
@@ -92,3 +91,29 @@ def get_tokenized_title(title):
 
     """
     return set(title.lower().split())
+
+
+def compute_title_score(x_title, y_title, threshold, math_threshold):
+    """Compute a score for a pair of titles.
+
+    Args:
+        x_title (string): one title.
+        y_title (string): the other title.
+        threshold (float): minimum overlap for the score to be non-zero in general.
+        math_threshold (float): minimum overlap for the score to be
+            non-zero in the presence of math in any of the titles.
+
+    Returns:
+        float: a score indicating the overlap of both titles, which is ``0`` if
+            below the threshold
+    """
+    x_title_tokens = get_tokenized_title(x_title)
+    y_title_tokens = get_tokenized_title(y_title)
+    current_title_jaccard = compute_jaccard_index(x_title_tokens, y_title_tokens)
+
+    some_title_has_math = (
+        "<math>" in x_title or "$" in x_title or "<math>" in y_title or "$" in y_title
+    )
+    current_threshold = math_threshold if some_title_has_math else threshold
+
+    return current_title_jaccard if current_title_jaccard >= current_threshold else 0.
