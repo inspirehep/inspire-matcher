13f32124ca738ee831c44901ed954722cf8fd390
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2025-05-05 10:32:44 +0200

    setup: correct version

--- setup.py
+++ setup.py
@@ -98,7 +98,7 @@ setup(
             "inspire_matcher = inspire_matcher:InspireMatcher",
         ],
     },
-    version="9.0.32",
+    version="9.0.36",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
